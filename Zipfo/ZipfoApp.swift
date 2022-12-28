da6e9ed7aafba3de693c2038a9a39d04ecda5a07
Justin Cabral <ijustincabral@gmail.com>
2022-12-28 09:23:56 -0500

    Initial Commit

---
+++ Zipfo/ZipfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZipfoApp.swift
+//  Zipfo
+//
+//  Created by Justin Cabral on 12/28/22.
+//
+
+import SwiftUI
+
+@main
+struct ZipfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
